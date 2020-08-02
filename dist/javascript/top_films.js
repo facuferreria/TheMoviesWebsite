78e83bfea60eb3f5524251239fe3ea6eaec8244f
facuferreria <60824330+facuferreria@users.noreply.github.com>
2020-08-02 18:49:09 -0300

    master

---
+++ dist/javascript/top_films.js
@@ -0,0 +1,65 @@
+'use strict';
+
+import { getImage, baseUrl, apiKey, showOptions } from "./app.js";
+
+const popularDiv = document.getElementById('popular-div');
+const kidsDiv = document.getElementById('kids-div');
+const theatreDiv = document.getElementById('theatre-div');
+
+const baseLocation = 'http://127.0.0.1:5500/dist/html%20pages/';
+
+export {popularDiv, kidsDiv, theatreDiv, showTopFilms}
+
+if (window.location.href === `${baseLocation}popular.html`) {
+    window.addEventListener('load', () => showTopFilms('&sort_by=popularity.desc', popularDiv));
+}
+
+if (window.location.href === `${baseLocation}theatre.html`) {
+    window.addEventListener('load', () => showTopFilms('&sort_by=release_date.desc', theatreDiv));
+}
+
+if (window.location.href === `${baseLocation}kids.html`) {
+    window.addEventListener('load', () => showTopFilms('&sort_by=popularity.desc&certification_country=US&certification=G', kidsDiv));
+}
+
+if (window.location.href === `${baseLocation}index.html`) {
+    window.addEventListener('load', ()=>  {return showTopFilms('&sort_by=release_date.desc')});   
+    window.addEventListener('load', ()=>  {return showTopFilms('&sort_by=popularity.desc')});
+    window.addEventListener('load', ()=> { return showTopFilms('&sort_by=popularity.desc&certification_country=US&certification=G')});
+}
+
+
+async function showTopFilms(type, container) {
+
+    const topUrl = `${baseUrl}discover/movie?api_key=${apiKey}${type}`;
+    let topRespose = await fetch(topUrl);
+    let topJson = await topRespose.json();
+    
+    if (window.location.href === `${baseLocation}index.html`) {
+        return showTopTree(topJson.results, type)
+    }
+
+    return showOptions(topJson.results, container);
+}
+
+
+function showTopTree  (data = [], typeFilms) {
+
+    let topPopular = document.getElementById('top-popular');
+    let topKids = document.getElementById('top-kids');
+    let topUpcomig = document.getElementById('top-upcoming');
+
+    let topThree = data.filter((el, i) => i < 4)
+    topThree.forEach((el, index) => {
+        
+        let topTxt = `
+        <div data-number-id = "${index}" class= "top-option">
+            <img src="${getImage('w185', el.poster_path)}" alt="poster film" class= "option-img">
+            <div class= "option-div"><h4 class= "option-title">${el.title}</h4></div>
+        </div>`;
+
+        if (typeFilms === '&sort_by=popularity.desc') return topPopular.insertAdjacentHTML('beforeend', topTxt);
+        if (typeFilms === '&sort_by=release_date.desc') return topUpcomig.insertAdjacentHTML('beforeend', topTxt);
+        else { topKids.insertAdjacentHTML('beforeend', topTxt) };
+    });
+}
